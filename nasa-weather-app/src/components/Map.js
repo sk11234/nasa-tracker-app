3087017074e801612c8651878a8827512358eb47
Sam K <sakw975257@gmail.com>
2022-06-22 20:53:15 -0500

    Finish by adding info boxes and header.

--- nasa-weather-app/src/components/Map.js
+++ nasa-weather-app/src/components/Map.js
@@ -1,7 +1,19 @@
+import {useState} from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
-const Map = ({center, zoom}) => {
+const Map = ({eventData, center, zoom}) => {
+  const [locationInfo, setLocationInfo] = useState(null)
+
+  const markers = eventData.map(ev => {
+    if (ev.categories[0].id === "wildfires") {
+      return <LocationMarker lat={ev.geometry[0].coordinates[1]} 
+      lng={ev.geometry[0].coordinates[0]} onClick={() => setLocationInfo({id: ev.id, title: ev.title})}/>
+    }
+    return null
+  })
+
   return (
     <div className='map'>
         <GoogleMapReact
@@ -10,8 +22,9 @@ const Map = ({center, zoom}) => {
             defaultCenter={center}
             defaultZoom={zoom}
         >
-            <LocationMarker lat={center.lat} lng={center.lng}/>
+          {markers}
         </GoogleMapReact>
+        {locationInfo && <LocationInfoBox info={locationInfo}/>}
     </div>
   )
 }
